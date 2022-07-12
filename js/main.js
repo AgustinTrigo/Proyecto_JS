68cc6419b44db59e1908f606c0e0b93a41e466b3
AgustinTrigo <a_trigo295@hotmail.com>
2022-07-12 20:27:42 -0300

    card-css_config

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 class Producto{
     constructor(id, nombre, precio, stock, cantidad, precioCantidad){
         this.id = id;
@@ -7,7 +8,62 @@ class Producto{
         this.cantidad = cantidad;
         this.precioCantidad = precioCantidad;
     }
-}
+}*/
 
 
-
+const inventario = [
+    {
+        id:1, 
+        nombre: "producto 1",
+        precio: 250,
+        stock: 20,
+        cantidad: 0,
+        precioCantidad: 0,
+        img: "./assets/img/productos/cepillos_dientes.jpg",
+    },
+    {
+        id:2, 
+        nombre: "producto 2",
+        precio: 250,
+        stock: 20,
+        cantidad: 0,
+        precioCantidad: 0,
+        img: "./assets/img/productos/esponja_vegetal_cocina.jp",
+    },
+    {
+        id:3, 
+        nombre: "producto 3",
+        precio: 250,
+        stock: 20,
+        cantidad: 0,
+        precioCantidad: 0,
+        img: "./assets/img/productos/maquina_afeitar.jpg",
+    },
+    {
+        id:4, 
+        nombre: "producto 4",
+        precio: 250,
+        stock: 20,
+        cantidad: 0,
+        precioCantidad: 0,
+        img: "./assets/img/productos/hilo_dental.jpg",
+    },
+    {
+        id:5, 
+        nombre: "producto 4",
+        precio: 250,
+        stock: 20,
+        cantidad: 0,
+        precioCantidad: 0,
+        img: "./assets/img/productos/pasta_dental.jpg",
+    },
+    {
+        id:6, 
+        nombre: "producto 6",
+        precio: 250,
+        stock: 20,
+        cantidad: 0,
+        precioCantidad: 0,
+        img: "./assets/img/productos/detergente_solido.jpg",
+    }
+]
