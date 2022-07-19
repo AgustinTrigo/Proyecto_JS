dc8771a8fdcb2dc5d7f39e023f4a225e043104cc
AgustinTrigo <a_trigo295@hotmail.com>
2022-07-18 23:59:55 -0300

    2da_entrega

--- js/main.js
+++ js/main.js
@@ -68,9 +68,10 @@ loadInventory();
 getInventory();
 
 let catalogo = document.getElementById("catalogo");
-let addBtn = document.getElementById("addBtn")
+let carritoCompras = document.getElementById("mainCart");
+let addBtn = document.getElementById("addBtn");
 catalogo.className = "galeria";
-
+carritoCompras.className = "styleCarrito";
 
 inventarioLocal.forEach((producto, index) => {
     catalogo.innerHTML += `
@@ -105,7 +106,31 @@ function addProduct(index){
         inventarioLocal[index].precioCantidad = inventarioLocal[index].cantidad * inventarioLocal[index].precio;
     }
     loadCart();
-    console.log(cart)
+    renderCarrito();
 }
+
+
+function renderCarrito(){
+    carritoCompras.innerHTML = "";
+    cart.forEach((i) => {
+        carritoCompras.innerHTML += `
+        <div class="carrito__card">
+            <div class="carrito__img">
+                <img src="${i.img}">
+            </div>
+            <div class="carrito__text">
+                <h5>${i.nombre}</h5>
+                <h5>precio $: ${i.precio}</h5>
+                <h5>unidades: ${i.cantidad}</h5>
+                <h5>subtotal $: ${i.precioCantidad}</h5>
+            </div>
+            <div class="carrito__btn">
+                <button type="button" value="" id="trashBtn" ><i class="fa-solid fa-trash-can"></i></button>
+            </div>
+        </div>`
+    })
+}
+
+
 
 
