b64b9ff90d85df1211c6e8d0a0526ed9cea92e04
AgustinTrigo(re) <a_trigo295@hotmail.com>
2022-07-26 15:18:00 -0300

    borre-inventario-local

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,9 +1,3 @@
-//const loadInventory = () => {localStorage.setItem("inventario", JSON.stringify(inventario))};
-const getInventory = () => {
-    return inventarioLocal = JSON.parse(localStorage.getItem("inventario")); 
-    
-}
-
 const loadCart = () => {localStorage.setItem("carrito", JSON.stringify(carritoLocal))};
 const getCart = () => {
     return carritoLocal = JSON.parse(localStorage.getItem("carrito")) || [];
--- js/main.js
+++ js/main.js
@@ -1,71 +1,61 @@
 getCart();
-console.log(carritoLocal);
 
 let catalogo = document.getElementById("catalogo");
 catalogo.className = "galeria";
+let select = document.getElementById("filtro");
 
 fetch('./js/inventario.json')
     .then((resultado) => resultado.json())
     .then((inventario) => {
-        inventario.forEach((producto, index) => {
-            catalogo.innerHTML += `
-            <div id="card" class="productCard">
-                <div class="productCard__img">
-                    <img src="${producto.img}">
-                </div>
-                <div class="productCard__text">
-                    <h5>${producto.nombre.toUpperCase()}</h5>
-                    <h5>precio contado</h5>
-                    <h5>$: ${producto.precio}</h5>
-                </div>
-                <div>
-                    <input type="button" value="AGREGAR" id="addBtn" class="productCard__btn" onClick="addProduct(${index})">
-                </div>
-            </div>`
-        })
+        if(select.value === "todos"){
+            inventario.forEach((producto, index) => {
+                catalogo.innerHTML += `
+                <div id="card" class="productCard">
+                    <div class="productCard__img">
+                        <img src="${producto.img}">
+                    </div>
+                    <div class="productCard__text">
+                        <h5>${producto.nombre.toUpperCase()}</h5>
+                        <h5>precio contado</h5>
+                        <h5>$: ${producto.precio}</h5>
+                    </div>
+                    <div>
+                        <input type="button" value="AGREGAR" id="addBtn" class="productCard__btn" onClick="addProduct(${index})">
+                    </div>
+                </div>`
+            })
+        }
     }) 
 
-getInventory().forEach((producto, index) => {
     
-    catalogo.innerHTML += `
-    <div id="card" class="productCard">
-        <div class="productCard__img">
-            <img src="${producto.img}">
-        </div>
-        <div class="productCard__text">
-            <h5>${producto.nombre.toUpperCase()}</h5>
-            <h5>precio contado</h5>
-            <h5>$: ${producto.precio}</h5>
-        </div>
-        <div>
-            <input type="button" value="AGREGAR" id="addBtn" class="productCard__btn" onClick="addProduct(${index})">
-        </div>
-    </div>`
-})
 
 function addProduct(index){
-    let findI = carritoLocal.findIndex(elemento =>{
-        return elemento.id === inventarioLocal[index].id;
-    })
-    if(findI === -1){
-        inventarioLocal[index].cantidad += 1;
-        inventarioLocal[index].precioCantidad = inventarioLocal[index].cantidad * inventarioLocal[index].precio;
-        carritoLocal.push(inventarioLocal[index]);
-    } else if(carritoLocal.length > 0){
-        carritoLocal[findI].cantidad += 1;
-        carritoLocal[findI].precioCantidad = carritoLocal[findI].cantidad * carritoLocal[findI].precio;
-    }
-    loadCart();
-    Toastify({
-        text: "PRODUCTO AGREGADO AL CARRITO",
-        duration: 1500,
-        position: "top",
-        position: "left",
-        style: {
-            background: 'linear-gradient(to top left, rgb(57, 175, 155), rgb(186, 228, 178))'
+    fetch('./js/inventario.json')
+        .then((resultado) => resultado.json())
+        .then((inventario) => {
+        let findI = carritoLocal.findIndex(elemento =>{
+            return elemento.id === inventario[index].id;
+        })
+        if(findI === -1){
+            inventario[index].cantidad += 1;
+            inventario[index].precioCantidad = inventario[index].cantidad * inventario[index].precio;
+            carritoLocal.push(inventario[index]);
+        } else if(carritoLocal.length > 0){
+            carritoLocal[findI].cantidad += 1;
+            carritoLocal[findI].precioCantidad = carritoLocal[findI].cantidad * carritoLocal[findI].precio;
         }
-    }).showToast();
-    sumarCantidad(carritoLocal);
+        loadCart();
+        Toastify({
+            text: "PRODUCTO AGREGADO AL CARRITO",
+            duration: 1500,
+            position: "top",
+            position: "left",
+            style: {
+                background: 'linear-gradient(to top left, rgb(57, 175, 155), rgb(186, 228, 178))'
+            }
+        }).showToast();
+        sumarCantidad(carritoLocal);
+        })
 }
 
 function sumarCantidad(array){
